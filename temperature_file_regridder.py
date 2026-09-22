77270d1f5826163c7bfdcf12aa2a65b27c45b902
omerrett <o.merrett@gmail.com>
2026-09-22 15:14:22 +0100

    Update temperature_file_regridder.py

--- temperature_file_regridder.py
+++ temperature_file_regridder.py
@@ -1,5 +1,5 @@
 """
-Due to OSTIA data being rectilinear and model data being curvilinear we must use  a regridder function to enable comparisons
+Due to OSTIA data being rectilinear and model data being curvilinear we must use  a regridder function to enable comparisons. This takes the input from monthly_and_annual_average.py.
 """
 
 import xarray as xr
@@ -10,10 +10,10 @@ import xesmf as xe
 ostia = xr.open_dataset('ostia_2018.nc')
 ostia_average_temp = ostia.mean(dim = 'time')
 
-annual_average_temp_og = xr.open_dataset('INPUT/DIRECTORY/annual_surface_temp/aowb_monthly_and_annual_surface_temp.nc')
-annual_average_temp_og.set_coords(['nav_lat','nav_lon'])
-annual_average_temp = annual_average_temp_og.rename({'nav_lon': 'lon', 'nav_lat': 'lat'})
-annual_sst = annual_average_temp['votemper']
+annual_average_temp_og = xr.open_dataset('INPUT/DIRECTORY/annual_surface_temp_aowb/aowb_monthly_and_annual_surface_temp.nc')
+annual_average_temp = annual_average_temp_og.set_coords(
+    ["nav_lat", "nav_lon"]
+).rename({"nav_lon": "lon", "nav_lat": "lat"})
 
 # Regridding function
 regridder = xe.Regridder(
@@ -23,17 +23,10 @@ regridder = xe.Regridder(
     ignore_degenerate=True
 )
 
-sst_regrid = regridder(annual_sst)
-
-
-# Ensure the array has a name (this becomes the variable name in the .nc file)
-sst_regrid.name = 'sst'
-
-# Convert to Dataset
-ds_output = sst_regrid.to_dataset()
+ds_regrid = regridder(annual_average_temp)
 
 # Save to NetCDF file
-output_path = "OUTPUT/DIRECTORY/annual_surface_temp/average_temp_REGRID.nc"
-ds_output.to_netcdf(output_path)
+output_path = "INPUT/DIRECTORY/annual_surface_temp_aowb/aowb_monthly_and_annual_surface_temp_REGRID.nc"
+ds_regrid.to_netcdf(output_path)
 
 print(f"Saved successfully to {output_path}")
